74d459e2cf776a703d96b761875c869bb2ac3c91
apccmfamador <cmfamador@student.apc.edu.ph>
2025-02-25 15:31:30 +0800

    vue initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <component :is="layout">
+      <router-view />
+    </component>
+  </template>
+  
+  <script setup>
+  import { computed } from 'vue'
+  import { useRoute } from 'vue-router'
+  import DefaultLayout from './layouts/DefaultLayout.vue'
+  
+  const route = useRoute()
+  
+  const layout = computed(() => {
+    // Get layout from route meta or fallback to default
+    return route.meta.layout || DefaultLayout
+  })
+  </script>
